baf8cf8ed403341dc90b1595a3dde4985b380e5f
Penouel aoga <penouelaoga@gmail.com>
2026-06-03 09:54:38 +0100

    fin

--- frontend/sw.js
+++ frontend/sw.js
@@ -5,18 +5,13 @@ const urlsToCache = [
   'style.css',
   'script.js',
   'manifest.json',
-  'libs/qrcode.js',
-  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=Bebas+Neue&display=swap',
-  'https://fonts.googleapis.com/icon?family=Material+Icons+Round',
-  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+  'libs/qrcode.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  // Force l'activation immédiate sans attendre la fermeture des onglets
   self.skipWaiting();
 });
 
@@ -28,7 +23,7 @@ self.addEventListener('activate', event => {
           if (cache !== CACHE_NAME) return caches.delete(cache);
         })
       );
-    }).then(() => self.clients.claim()) // Prend le contrôle immédiatement
+    }).then(() => self.clients.claim())
   );
 });
 
